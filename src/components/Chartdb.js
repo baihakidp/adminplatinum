4b4662dd841307d673af4a001b8f109e4a4f231a
gaming <baihakidemiandraputra@gmail.com>
2023-02-13 21:51:14 +0700

    add chart and add new button

--- src/components/Chartdb.js
+++ src/components/Chartdb.js
@@ -1,5 +1,8 @@
 import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { Form } from "react-bootstrap";
 import { FiChevronRight } from "react-icons/fi";
+import chartData from "../const/chartData";
 import "./Chart.css";
 
 const Chartdb = () => {
@@ -23,12 +26,38 @@ const Chartdb = () => {
         </div>
       </div>
       <div className="dasboard-month-bg">
-        <div>
+        <div className="dashboard-month-title">
           <p>Month</p>
         </div>
-        <div>
-          <div></div>
+        <div className="dashboard-month-select-bg">
+          <div className="dashboard-month-select">
+            <Form.Group>
+              <Form.Select>
+                <option>Januari - 2022</option>
+                <option>Februari - 2022</option>
+                <option>Maret - 2022</option>
+                <option>April - 2022</option>
+                <option>Mei - 2022</option>
+                <option>Juni - 2022</option>
+                <option>Juli - 2022</option>
+                <option>Agustus - 2022</option>
+                <option>September - 2022</option>
+                <option>Oktober - 2022</option>
+                <option>November - 2022</option>
+                <option>Desember - 2022</option>
+              </Form.Select>
+            </Form.Group>
+          </div>
+          <div className="dashboard-month-btn">
+            <button>Go</button>
+          </div>
         </div>
+        <ReactApexChart
+          options={chartData}
+          series={chartData.series}
+          width="96%"
+          height="376px"
+        />
       </div>
     </div>
   );
