af546d552b20ea9f75cf8e1298366637f351f628
gaming <baihakidemiandraputra@gmail.com>
2023-02-17 21:08:20 +0700

    fix login and chart

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,15 +1,6 @@
-import axios from "axios";
-
 export const authAction = (payload) => (dispatch) => {
-  axios
-    .post("https://bootcamp-rent-cars.herokuapp.com/admin/auth/login", payload)
-    .then((res) => {
-      localStorage.setItem("token", res.data.access_token);
-      localStorage.setItem("email", res.data.email);
-      dispatch({
-        type: "LOGIN",
-        payload: true,
-      });
-    })
-    .catch((err) => console.log(err));
+  dispatch({
+    type: "LOGIN",
+    payload: true,
+  });
 };
